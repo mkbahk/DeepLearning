8b50c3e144dd07f56640beabe515922a7514321f
Moon-Kee Bahk <mkbahk@gmail.com>
2021-02-06 13:27:50 +0900

    Some python script added

---
+++ ClassStaticMethod.py
@@ -0,0 +1,14 @@
+class Calc:
+    @staticmethod
+    def add(a, b):
+        print(a + b)
+    ###end of def
+
+    @staticmethod
+    def mul(a, b):
+        print(a * b)
+    ###end of def
+###end of class
+
+Calc.add(10, 20)
+Calc.mul(10, 20)
